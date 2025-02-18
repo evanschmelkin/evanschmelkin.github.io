6cfe3ee6d4c91d4b684dd84598cdbe738eccbe36
evanschmelkin <evanschmelkin@gmail.com>
2025-02-18 12:12:33 -0500

    edited javascript

---
+++ example.js
@@ -0,0 +1,5 @@
+document.write( 'The Output String' ) // output to the HTML document
+window.alert( 'The Alert Text' ) // output to a separate alert window
+confirm('Confirm or cancel') // displays text; returns OK (T) or cancel (F)
+prompt('Enter your age', 21) // get input from the user
+console.log('THE ERROR')
